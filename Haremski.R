188afafe1aef95b28c88a9c6a1d248207696e12b
TheWojciech <wojhar@st.amu.edu.pl>
2021-03-29 19:32:56 +0200

    Tyle potrafię

---
+++ Haremski.R
@@ -0,0 +1,15 @@
+#1
+library(stringr)
+tekst1 = "TERYT 18; podkarpackie; Rzeszów; 0.2 He; A"
+tekst2 = "TERYT 22; pomorskie;   Gdańsk; 12 C ; B"
+ str_sub(tekst1, start = -5, end = -4)
+
+ str_sub(tekst2, start = -5, end = -4)
+
+#2
+ horoskop = function(imie, miesiac){
+   if((miesiac %% 2) == 0){
+     cat(paste("Osoba o imieniu", imie, "będzie miała jutro szczęście"))
+         }else(cat(paste("Osoba o imieniu", imie, "będzie miała jutro nieszczęście")))
+   r.na = TRUE
+    }
