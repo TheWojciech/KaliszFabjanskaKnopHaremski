bef11f42985186aeb4fc99ba9398d462e0616073
TheWojciech <wojhar@st.amu.edu.pl>
2021-03-29 19:38:09 +0200

    Tyle potrafię

--- Haremski.R
+++ Haremski.R
@@ -3,9 +3,7 @@ library(stringr)
 tekst1 = "TERYT 18; podkarpackie; Rzeszów; 0.2 He; A"
 tekst2 = "TERYT 22; pomorskie;   Gdańsk; 12 C ; B"
  str_sub(tekst1, start = -5, end = -4)
-
  str_sub(tekst2, start = -5, end = -4)
-
 #2
  horoskop = function(imie, miesiac){
    if((miesiac %% 2) == 0){
